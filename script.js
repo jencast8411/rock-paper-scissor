74e942aa166d4bbeb914dcefdc74d06bf5ebbe55
Jenally Castaneda <castaneda.jenally@gmail.com>
2023-02-09 12:39:04 -0500

    Added notes to explain functions

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
+// Set global variables where functions can pull values from. For homework, I think we will have to input all the characters we need (numbers, letters, special characters)
 var pcOptions = ['R', 'P', 'S'];
 var playerScore = 0;
 var computerScore = 0;
 
-function getPcAnswer() {
-    return pcOptions[Math.floor(Math.random() * pcOptions.length)];
-}
-
+// First step: Prompt player to make selection
 // prompt allows player to input answer
 function getPlayerAnswer() {
     return prompt("Enter R for Rock, P for Paper, S for Scissors").toUpperCase();
 }
 
-// alert is only a pop-up; not interactive
+// Second step: Computer generates random choice
+function getPcAnswer() {
+    return pcOptions[Math.floor(Math.random() * pcOptions.length)];
+}
+
+//Third step: Compare answers with if/else 
 function compareAnswers(playerAnswer, pcAnswer) {
     if (playerAnswer === pcAnswer) {
         alert("It's a Tie!");
@@ -30,10 +33,12 @@ function compareAnswers(playerAnswer, pcAnswer) {
     }
 }
 
+// Fourth step: tally scores
 function displayScore() {
     alert("Player score: " + playerScore + " Computer score: " + computerScore);
 }
 
+// So THIS part.... I got from Google so NOOO idea, but it's to compare scores. It looks like a nested function with new vars? 
 while (true) {
     var pcAnswer = getPcAnswer();
     var playerAnswer = getPlayerAnswer();
